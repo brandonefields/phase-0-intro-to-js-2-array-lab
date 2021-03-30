a9176ea8b98be39f1b998f94e3654dc9bf98287f
BrandonFields <brandonefields@gmail.com>
2021-03-30 13:35:31 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+
 // Write your solution here!
+
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+};
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+};
+
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+};
+
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+};
+
+function appendCat(name){
+   return [...cats, name];
+};
+
+function prependCat(name){
+    const newCats = [...cats];
+    newCats.unshift(name)
+    return newCats;
+};
+ 
+function removeLastCat (){
+    const newCats = [...cats];
+    newCats.pop()
+    return newCats
+};
+
+function removeFirstCat(){
+    const newCats = [...cats];
+    newCats.shift()
+    return newCats
+};
